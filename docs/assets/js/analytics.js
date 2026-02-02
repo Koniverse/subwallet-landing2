669d39a0f618878ef564aa7b6a793276388b7110
frenkie <dungtpa2@gmail.com>
2026-02-02 15:42:01 +0700

    [Issue-7] security (script-src in CSP) - Refactor Google Analytics script - Restrict Google endpoint from `https://www.googletagmanager.com` to `https://www.googletagmanager.com/gtag/js`

---
+++ docs/assets/js/analytics.js
@@ -0,0 +1,7 @@
+// Google Analytics Configuration
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-PT1N37CDQ4');
